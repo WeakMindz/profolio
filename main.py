24d87223cb925d35975e75f5e22f91181571a3fa
Chol <thekingflame94@gmail.com>
2026-07-15 23:11:07 -0500

    Added a new feature

--- main.py
+++ main.py
@@ -3,6 +3,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Home"
+    return render_template("home.html")
+    #Quick Main information about me
+@app.route("/about")
+def about():
+    return "about"
+    #Tells more about me and what I do 
+
+@app.route('/project')
+def project():
+    return render_template("about.html")
+    #Projects such as my autoclikcer 
+    #Also need to do more projects
+    #MAke anime website as a project
+
+
+@app.route("/achivements")
+def achivements():
+    return render_template("achivements.html")
+    #Things that I did in robotics and coding competion over 11th and 12th grade.
+
+
+@app.route("/goals")
+def goals():
+    return "goals"
+    #List my goals and things I want to do
 if __name__ == '__main__':
     app.run(debug=True)
